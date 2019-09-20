fdb6aaefeb59db7cf1e8105c0d6ff319f160c6d5
LozovoyV <lozovoyv@gmail.com>
2019-09-20 13:37:18 +0300

    Fix module name and path

--- src/SiteMap.php
+++ src/SiteMap.php
@@ -6,5 +6,9 @@ use Core\Foundation\Module\BaseModule;
 
 class SiteMap extends BaseModule
 {
+    /** @var string  Module name */
+    protected $name = 'opx_site_map';
 
+    /** @var string  Module path */
+    protected $path = __DIR__;
 }
