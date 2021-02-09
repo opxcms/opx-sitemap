aba9563ae6d956c6a8adf5e05ebb9894417f20d6
lozovoyv <lozovoyv@gmail.com>
2021-02-09 16:00:59 +0300

    updates

--- src/SiteMap.php
+++ src/SiteMap.php
@@ -11,4 +11,25 @@ class SiteMap extends BaseModule
 
     /** @var string  Module path */
     protected $path = __DIR__;
+
+    /**
+     * Override load config.
+     */
+    protected function loadConfig(): void
+    {
+        if (file_exists($filename = $this->app->basePath('templates/Opx/SiteMap/config.php'))) {
+
+            $this->config = require $filename;
+
+        } else if (file_exists($filename = "{$this->path}/config.php")) {
+
+            $this->config = require $filename;
+
+        } else {
+
+            $this->config = [];
+        }
+    }
+
+
 }
